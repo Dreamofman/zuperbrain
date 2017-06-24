d3fb66d96f11d07afc081f57d8c4a0f19c3c18bb
dreamofman <deep.skyper@gmail.com>
2017-06-24 11:52:34 +0700

    edit on zuperbrain

--- scripts/base.js
+++ scripts/base.js
@@ -1,14 +1,30 @@
 $(document).ready(function() {
    
-     updateCurrency()
+    updateCurrency()
+    $('#btnSubmit').click(function( event ) 
+    {
+        var sel1Values = $( "#sel1" ).val();
 
+          updateCurrency(sel1Values)
+       
+    });
+    /*$('#sel1').change(function(event) {
+        var sel1Values = $( "#sel1" ).val();
+        updateCurrency(sel1Values)
+
+        
+
+    });*/
+   /*document.getElementById('btnSubmit').addEventListener('click', function() {
+         updateCurrency('THB')
+    });*/
 });
 
-function updateCurrency()
+function updateCurrency(cur='EUR')
 {
   $.ajax({
        type: "GET",
-       url: "http://api.fixer.io/latest?base=EUR",
+       url: "http://api.fixer.io/latest?base="+cur,
        data: {},
        success: function(result) 
        {
@@ -17,16 +33,24 @@ function updateCurrency()
         x = result.base;
         y = result.date;
         z = result.rates;
-        $('#base').html('<div class="col-xs-12 col-sm-12 col-md-12">'+x+'</div>')
-        $('#date').html('<div class="col-xs-12 col-sm-12 col-md-12">'+y+'</div>')
-            var allRates=''
+        var allRates=''
+        var allSels='';
             for (v in z) 
             {
               allRates+= '<div class="col-xs-6 col-sm-4">'+v+':'+z[v]+'</div>'
+              if(v===cur){
+                  allSels += '<option value='+v+' selected="selected">'+v+'</option>'
+              }else{
+                  allSels += '<option value='+v+'>'+v+'</option>'
+              }
               
             }
+        //$('#base').html('<div class="col-xs-12 col-sm-12 col-md-12">'+x+'</div>')
+        $('#sel1').html(allSels)
+        $('#date').html('<div class="col-xs-12 col-sm-12 col-md-12"><h1>'+y+'</h1></div>')
         $('#rates').html(allRates)
         //$('pre').text(allRates);
+
        },
        error: function(x, e) 
        { 
